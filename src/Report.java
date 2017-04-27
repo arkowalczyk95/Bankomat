3c538f2d68fd1acc3d7471fd07a252be7ad66174
arkowalczyk95 <arkowalczyk95@gmail.com>
2017-04-27 17:03:59 +0200

    2

---
+++ src/Report.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+/**
+ * Created by Administrator on 2017-04-27.
+ */
+public class Report {
+    public void write() throws FileNotFoundException {
+        File file = new File("Report.txt");
+        PrintWriter zapis = new PrintWriter("Report.txt");
+
+    }
+}
